e1fb554b75f63e4e7865c14a09e7c2f51d3b6026
Elie <eliemhess@gmail.com>
2021-08-15 10:32:12 -0700

    Core functionality

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,35 @@ impl Card<'_> {
     pub fn new<'a>(name: &'a str, quantity: i32) -> Card {
         Card {name, quantity}
     }
+
+    pub fn get_name(&self) -> &'_ str {
+        self.name
+    }
+
+    pub fn get_quantity(&self) -> i32 {
+        self.quantity
+    }
+
+    pub fn to_string(&self) -> String {
+        format!("{} {}", self.quantity, self.name)
+    }
 }
 
 impl fmt::Display for Card<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {}", self.quantity, self.name)
+        write!(f, "{}", self.to_string())
     }
+}
+
+pub fn sort(mut list: Vec<Card>) -> Vec<Card> {
+    list.sort_by(|a, b| a.name.cmp(b.name));
+    list
+}
+
+pub fn join(list: &Vec<Card>) -> String {
+    let mut str_vec = Vec::new();
+    for card in list {
+        str_vec.push(card.to_string())
+    }
+    str_vec.join("\n")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 use alphabetizer_rust::Card;
+use clipboard_win::Clipboard;
 
 fn main() {
-    let card = Card::new("hi", 3);
-    println!("{}", card);
+    let raw_input = Clipboard::new().unwrap().get_string().unwrap();
+    let lines: Vec<&str> = raw_input.lines().collect();
+    
+    let mut cards: Vec<Card> = Vec::new();
+    for line in lines {
+        let index: usize;
+        if let Some(a) = line.find(" ") {
+            index = a;
+        } else {
+            println!("ERROR: No spaces found in line");
+            break;
+        }
+        let quantity: i32 = line[0..index].parse::<i32>().unwrap();
+        let name: &str = &line[index+1..line.len()];
+        cards.push(Card::new(name, quantity));
+    }
+
+    cards = alphabetizer_rust::sort(cards);
+
+    Clipboard::new().unwrap().set_string(&alphabetizer_rust::join(&cards)).unwrap();
 }
