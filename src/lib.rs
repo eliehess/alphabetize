0168d5b369844f74756c5f4c834f15450da716bf
Elie <eliemhess@gmail.com>
2021-08-15 18:45:31 -0700

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -54,13 +54,13 @@ pub mod card {
                 index = a;
             } else {
                 errors.push(String::from("No spaces found in line"));
-                break;
+                continue;
             }
             let quantity: i32 = match line[0..index].parse::<i32>() {
                 Ok(val) => val,
                 Err(e) => {
-                    errors.push(format!("Unable to parse quantity - {}", e));
-                    break;
+                    errors.push(format!("{}", e));
+                    continue;
                 }
             };
             let name: &str = &line[index+1..line.len()];
@@ -68,5 +68,71 @@ pub mod card {
         }
 
         return (cards, errors);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::card::*;
+
+    #[test]
+    fn empty_input() {
+        let (cards, errors) = parse("");
+        assert_eq!(cards.len(), 0);
+        assert_eq!(errors.len(), 0);
+    }
+
+    #[test]
+    fn correct_parse() {
+        let (cards, errors) = parse("4 Thoughtseize\n4 Brainstorm");
+        assert_eq!(cards.len(), 2);
+        assert_eq!(cards[0].get_name(), "Thoughtseize");
+        assert_eq!(cards[1].get_name(), "Brainstorm");
+        assert_eq!(errors.len(), 0);
+    }
+
+    #[test]
+    fn correct_parse_with_trailing_newline() {
+        let (cards, errors) = parse("4 Thoughtseize\n4 Brainstorm\n");
+        assert_eq!(cards.len(), 2);
+        assert_eq!(cards[0].get_name(), "Thoughtseize");
+        assert_eq!(cards[1].get_name(), "Brainstorm");
+        assert_eq!(errors.len(), 0);
+    }
+
+    #[test]
+    fn no_nums() {
+        let (cards, errors) = parse("Thoughtseize\n4 Brainstorm");
+        assert_eq!(cards.len(), 1);
+        assert_eq!(cards[0].get_name(), "Brainstorm");
+        assert_eq!(errors.len(), 1);
+        assert_eq!(errors[0], String::from("No spaces found in line"));
+    }
+
+    #[test]
+    fn no_spaces() {
+        let (cards, errors) = parse("4Thoughtseize\n4 Brainstorm");
+        assert_eq!(cards.len(), 1);
+        assert_eq!(cards[0].get_name(), "Brainstorm");
+        assert_eq!(errors.len(), 1);
+        assert_eq!(errors[0], String::from("No spaces found in line"));
+    }
+    
+    #[test]
+    fn not_numbers() {
+        let (cards, errors) = parse("a Thoughtseize\n4 Brainstorm");
+        assert_eq!(cards.len(), 1);
+        assert_eq!(cards[0].get_name(), "Brainstorm");
+        assert_eq!(errors.len(), 1);
+        assert_eq!(errors[0], String::from("invalid digit found in string"));
+    }
+
+    #[test]
+    fn sorts_cards() {
+        let mut cards = vec!(Card::new(4, "Thoughtseize"), Card::new(4, "Brainstorm"));
+        sort(&mut cards);
+        assert_eq!(cards.len(), 2);
+        assert_eq!(cards[0].get_name(), "Brainstorm");
+        assert_eq!(cards[1].get_name(), "Thoughtseize");
     }
 }
